3125c21d2a7eb6c5570d01883b0accbb2e04b738
pedrofonseca1227 <henrriquemaluko@gmail.com>
2025-06-04 10:37:54 -0300

    Fix alias '@' resolution for Netlify build

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
     plugins: [
-      new (require('webpack')).DefinePlugin({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true)
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
       }),
     ],
   },
